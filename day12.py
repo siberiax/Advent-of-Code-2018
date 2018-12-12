950aa59e6a17b2033a596ca54d086ca5ef8852e8
Alex McHugh <siberia@Alexs-MacBook-Pro.local>
2018-12-12 02:07:01 -0500

    tidied up code

--- day12.py
+++ day12.py
@@ -25,29 +25,19 @@ def stabilize_plants():
         curr_plants[lowest] = '.'
         highest += 1
         curr_plants[highest] = '.'
-    return (lowest,highest)
+    return lowest,highest
 
 prev_new = defaultdict(chr)
-i = 0
 for i in range(1000):
     new_state = defaultdict(chr)
-    pos = stabilize_plants()
-    start = pos[0]
-    end = pos[1]
+    start, end = stabilize_plants()
     for x in range(start+2,end-2):
         segment = curr_plants[x-2] + curr_plants[x-1] + curr_plants[x] + curr_plants[x+1] + curr_plants[x+2]
         new_state[x] = state_changes[segment]
-    p1 = ""
-    for el in sorted(prev_new.keys()):
-        p1 += curr_plants[el]
-    p2 = ""
-    for el in sorted(new_state.keys()):
-        p2 += new_state[el]
-    if p1 == p2:
-        curr_plants = new_state
+    curr_plants = new_state
+    if prev_new.values() == new_state.values():
         break
     prev_new = new_state
-    curr_plants = new_state
 
 final = 0
 for el in curr_plants.keys():
